13f86aab39e189920658ef27fc14a0518ec285ec
dianprnms <purnamasaridian010@gmail.com>
2023-03-08 19:48:19 +0700

    Bilangan Prima

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,7 @@
+fun main(args: Array<String>) {
+    val n = 2 // Angka yang ingin diperiksa
+    val primaa = Prima(2)
+    primaa.sum()
+
+
+}
---
+++ src/main/kotlin/Prima.kt
@@ -0,0 +1,9 @@
+class Prima (var angka : Int){
+    fun sum() {
+        if((angka / angka == 1) || (angka % 2 == 1)){
+            print("Prima")
+        }else{
+            print("Bukan Prima")
+        }
+    }
+}
